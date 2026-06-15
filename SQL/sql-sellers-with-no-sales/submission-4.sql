9ee8eedd5117615231f9df24be05d2a849ad52b2
Lakshya Nagpal <lnagpal15@gmail.com>
2026-06-16 04:50:39 +0530

    Add: sql-sellers-with-no-sales - submission-4

---
+++ SQL/sql-sellers-with-no-sales/submission-4.sql
@@ -0,0 +1,5 @@
+-- Write your query below
+select s.seller_name from seller s 
+where not exists ( select 1 from orders o where
+o.seller_id=s.seller_id and extract(YEAR FROM o.sale_date) = 2020 
+) order by seller_name
